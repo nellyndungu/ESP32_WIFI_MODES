2d31ce6c0714d6f1905dcafdb9b4f22e199fecfd
nellyndungu <nellywambui060@gmail.com>
2025-09-12 13:31:15 +0300

    Access Point Code

---
+++ ESP32_AP_MODE/ESP32_AP_MODE.ino
@@ -0,0 +1,47 @@
+// Access point - the stations/devices connect to ESP32
+// Good for wireless communication between one ESP32 and other devices without needing a router. 
+// Include WiFi library
+#include <WiFi.h>
+#include "esp_wifi.h"
+
+// Set network credentials
+const char* ssid = "ESP32_AP";
+const char* password = "@2025";
+
+void setup(){
+  Serial.begin(115200);
+
+  Serial.print("Setting AP ...");
+  //Start ESP32 in Access Point Mode
+  WiFi.softAP(ssid, password);
+
+  // Print network details
+  Serial.println("Access Point Initiated!");
+  Serial.println(ssid);
+  Serial.print(": ");
+  Serial.print(WiFi.softAPIP());
+}
+void loop(){
+// Get number of stations connected
+int stationsNum = WiFi.softAPgetStationNum();
+
+//Print the number of connected stations
+Serial.print("Connected devices: ");
+Serial.println(stationsNum);
+
+if(stationsNum > 0){
+  wifi_sta_list_t stationList;
+  esp_wifi_ap_get_sta_list (&stationList);
+  Serial.printf("Connected devices: %d\n", stationList.num);
+
+for (int i = 0; i < stationList.num; i++) {
+    wifi_sta_info_t station = stationList.sta[i];
+
+     Serial.printf("Device %d - MAC: %02X:%02X:%02X:%02X:%02X:%02X\n",
+                  i + 1,
+                  station.mac[0], station.mac[1], station.mac[2],
+                  station.mac[3], station.mac[4], station.mac[5]);
+  }
+}
+delay(500);
+}
